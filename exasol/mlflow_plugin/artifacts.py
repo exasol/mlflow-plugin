b1ff7519b413ca908a13086acea33c91698fd8b0
ckunki <christoph.kuhnke@exasol.com>
2026-01-20 17:59:00 +0100

    Ignore sonar findings for now

--- exasol/mlflow_plugin/artifacts.py
+++ exasol/mlflow_plugin/artifacts.py
@@ -25,9 +25,8 @@ class BucketFsArtifactRepo(ArtifactRepository):
             "BucketFsArtifactRepo.log_artifacts("
             f"local_dir={local_dir}, artifact_path={artifact_path})"
         )
-        import subprocess
-
-        subprocess.run(["ls", "-l", local_dir])
+        import subprocess # nosec: B404
+        subprocess.run(["ls", "-l", local_dir]) # nosec: B603, B607
         # Upload directory to your storage system
 
     def list_artifacts(self, path=None):
