1ebc0f3a2f2bae7af50a3063f81e22e9041fdb80
ckunki <christoph.kuhnke@exasol.com>
2026-01-21 14:25:38 +0100

    updated import

--- exasol/mlflow_plugin/artifacts/bucketfs_spec.py
+++ exasol/mlflow_plugin/artifacts/bucketfs_spec.py
@@ -10,13 +10,14 @@ Prefixes:
 
 import os
 from pathlib import Path
-from urllib.parse import (
-    ParseResult,
-    urlparse,
-)
+from urllib.parse import urlparse
 
-from exasol.mlflow_plugin import env_vars
-from exasol.mlflow_plugin.env_vars import str_to_bool
+from exasol.mlflow_plugin.env_vars import (
+    ENV_BUCKETFS_PASSWORD,
+    ENV_BUCKETFS_USER,
+    ENV_SSL_CERT_VALIDATION,
+    str_to_bool,
+)
 
 URL_SCHEMES = ["bfs", "bfss"]
 
@@ -28,7 +29,7 @@ class BfsSpecError(Exception):
     """
 
 
-def parse_url(artifact_root: str) -> ParseResult:
+def parse_url(artifact_root: str) -> tuple[str, str, str, str]:
     url = urlparse(artifact_root)
     if url.scheme not in ["bfs", "bfss"]:
         raise BfsSpecError(f'Artifact_root "{artifact_root}" is not in {URL_SCHEMES}.')
@@ -38,28 +39,29 @@ def parse_url(artifact_root: str) -> ParseResult:
             f'Artifact_root "{artifact_root}" must contain'
             " BucketFS service name and bucket name."
         )
-    return url
 
-
-def bucketfs_parameters(artifact_root: str) -> dict[str, str | bool]:
-    url = parse_url(artifact_root)
     parts = Path(url.path).parts
     service = parts[1]
     bucket = parts[2]
     rest = parts[3:]
     path = "/".join(rest) if rest else ""
     protocol = "https" if url.scheme == "bfss" else "http"
-    bfs_write_user = os.getenv(env_vars.ENV_BUCKETFS_USER, "w")
-    password = os.getenv(env_vars.ENV_BUCKETFS_PASSWORD)
+    return (f"{protocol}://{url.netloc}", service, bucket, path)
+
+
+def bucketfs_parameters(artifact_root: str) -> dict[str, str | bool]:
+    url, service, bucket, path = parse_url(artifact_root)
+    bfs_write_user = os.getenv(ENV_BUCKETFS_USER, "w")
+    password = os.getenv(ENV_BUCKETFS_PASSWORD)
     if not password:
         raise BfsSpecError(
-            f"Environment variable {env_vars.ENV_BUCKETFS_PASSWORD} must be"
+            f"Environment variable {ENV_BUCKETFS_PASSWORD} must be"
             " set to the write password for uploading files to the BucketFS."
         )
-    verify = os.getenv(env_vars.ENV_SSL_CERT_VALIDATION, "true")
+    verify = os.getenv(ENV_SSL_CERT_VALIDATION, "true")
     return {
         "backend": "onprem",
-        "url": f"{protocol}://{url.netloc}",
+        "url": url,
         "username": bfs_write_user,
         "password": password,
         "service_name": service,
