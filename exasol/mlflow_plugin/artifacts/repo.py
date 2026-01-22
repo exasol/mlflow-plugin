2ff349a582c792012cc356e02b876e02af3894c3
ckunki <christoph.kuhnke@exasol.com>
2026-01-22 18:35:29 +0100

    Implemented list_artifacts, _download_file

--- exasol/mlflow_plugin/artifacts/repo.py
+++ exasol/mlflow_plugin/artifacts/repo.py
@@ -1,18 +1,15 @@
+import logging
 import os
-import subprocess
-from typing import (
-    Any,
-    List,
-)
+import posixpath
 
 import exasol.bucketfs as bfs
+from mlflow.entities import FileInfo
 from mlflow.store.artifact.artifact_repo import ArtifactRepository
 from mlflow.utils.file_utils import relative_path_to_artifact_path
 
 from exasol.mlflow_plugin import connections
 from exasol.mlflow_plugin.artifacts.bucketfs_spec import bucketfs_parameters
 
-import logging
 
 def bfs_location(artifact_uri: str) -> bfs.path.PathLike:
     params = bucketfs_parameters(artifact_uri)
@@ -24,7 +21,7 @@ logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
 
 
 class BucketFsArtifactRepo(ArtifactRepository):
-    """Custom artifact repository for scheme 'bfs://'"""
+    """Custom artifact repository for scheme 'exa+bfs://'"""
 
     def __init__(
         self,
@@ -32,10 +29,9 @@ class BucketFsArtifactRepo(ArtifactRepository):
         tracking_uri: str | None = None,
         registry_uri: str | None = None,
     ) -> None:
-        # Initialize your artifact storage backend
         # artifact_uri=bfs://localhost:2580/bfsdefault/default/my_path/0/models/m-e2506c0fa86d43aa8352749b9980c4ec/artifacts
         super().__init__(artifact_uri)
-        # self._bfs = bfs_location(artifact_uri)
+        self._bfs = bfs_location(artifact_uri)
         self._log("__init__", artifact_uri=artifact_uri)
 
     def _log(self, file_name: str, **kwargs) -> None:
@@ -46,12 +42,18 @@ class BucketFsArtifactRepo(ArtifactRepository):
         LOG.info("%s.%s(%s)", type(self).__name__, file_name, arg_str)
 
     def log_artifact(self, local_file, artifact_path=None):
-        # Upload file to your storage system
+        """
+        Upload file to BucketFS
+
+        Sample args:
+        * local_file: "/tmp/tmptr2u6e0y/model/requirements.txt"
+        * artifact_path: None
+        """
         self._log("log_artifact", local_file=local_file, artifact_path=artifact_path)
-        # parent = self._bfs / artifact_path if artifact_path else self._bfs
-        # dest = parent / os.path.basename(local_file)
-        # with open(local_file, "rb") as fd:
-        #     dest.write(fd)
+        parent = self._bfs / artifact_path if artifact_path else self._bfs
+        dest = parent / os.path.basename(local_file)
+        with open(local_file, "rb") as fd:
+            dest.write(fd)
 
     def _child_path(self, root: str, local_dir: str, artifact_path: str | None) -> str:
         """
@@ -68,23 +70,67 @@ class BucketFsArtifactRepo(ArtifactRepository):
         return posixpath.join(artifact_path, rel) if artifact_path else rel
 
     def log_artifacts(self, local_dir, artifact_path=None):
+        """
+        Upload all files in the specified directory to BucketFS
+
+        Sample args:
+        * local_dir: "/tmp/tmptr2u6e0y/model"
+        * artifact_path: None
+        """
+
         self._log("log_artifacts", local_dir=local_dir, artifact_path=artifact_path)
         for root, _, files in os.walk(local_dir):
             for f in files:
                 path = self._child_path(root, local_dir, artifact_path)
                 self.log_artifact(os.path.join(root, f), path)
 
-        # import subprocess  # nosec: B404
-        # subprocess.run(["ls", "-l", local_dir])  # nosec: B603, B607
-        # Upload directory to your storage system
+    def list_artifacts(self, path=None) -> list[FileInfo]:
+        """
+        List artifacts in BucketFS
 
-    def list_artifacts(self, path=None):
-        # List artifacts in your storage system
-        print(f"BucketFsArtifactRepo.list_artifacts(path={path})")
+        Sample args:
+        * path: "python_env.yaml"
+        """
+        self._log("list_artifacts", path=path)
 
-    def download_artifacts(self, artifact_path, dst_path=None):
-        # Download artifacts from your storage system
-        print(
-            "BucketFsArtifactRepo.download_artifacts("
-            f"artifact_path={artifact_path}, dst_path={dst_path})"
+        def info(root: bfs.path.PathLike, name: str, is_dir: bool):
+            # mlflow's default http_artifact_repo.py uses
+            # mlflow.utils.uri.validated_path here to prevent path traversal
+            # attacks with ".." etc.
+            fpath = posixpath.join(path, name) if path else name
+            LOG.info(f'- %s', fpath)
+            return FileInfo(path=fpath, is_dir=is_dir, file_size=None)
+
+        def dir_info(root: bfs.path.PathLike, name: str):
+            return info(root, name, is_dir=True)
+
+        def file_info(root: bfs.path.PathLike, name: str):
+            return info(root, name, is_dir=False)
+
+        bfsloc = self._bfs / path if path else self._bfs
+        result = []
+        for root, dirs, files in bfsloc.walk():
+            result += [dir_info(root, x) for x in dirs]
+            result += [file_info(root, x) for x in files]
+
+        return result
+
+    # download_artifacts() is already implemented by class ArtifactRepository,
+    # but BucketFsArtifactRepo needs to implement the abstractmethod
+    # _download_file().
+
+    def _download_file(self, remote_file_path, local_path):
+        """
+        Download the specified file from BucketFS to the local filesystem.
+
+        Sample args:
+        * remote_file_path: "requirements.txt",
+        * local_path: "/tmp/tmp4cdl_a_5/requirements.txt")
+        """
+        self._log(
+            "_download_file",
+            remote_file_path=remote_file_path,
+            local_path=local_path,
         )
+        bfsloc = self._bfs / remote_file_path
+        bfs.as_file(bfsloc.read(), local_path)
--- exasol/mlflow_plugin/experiment/main.py
+++ exasol/mlflow_plugin/experiment/main.py
@@ -2,9 +2,12 @@ import logging
 
 import mlflow
 from sklearn.linear_model import LogisticRegression
+from mlflow.entities import FileInfo
 
 from exasol.mlflow_plugin.artifacts.bucketfs_spec import bucketfs_parameters
 from exasol.mlflow_plugin.connections import bucketfs_location
+from exasol.mlflow_plugin.artifacts.repo import bfs_location
+import exasol.bucketfs as bfs
 
 mlflow.set_tracking_uri("http://localhost:5000")
 
@@ -25,8 +28,12 @@ def store_model():
     lr.fit(td.X_train, td.y_train)
 
 
-def upload_to_bucketfs(file: str):
-    params = bucketfs_parameters("bfs://localhost:2580/bfsdefault/default")
+def upload_to_bucketfs():
+    import sys
+    file = sys.argv[1]
+    print(f'uploading {file}')
+
+    params = bucketfs_parameters("exa+bfs://localhost:2580/bfsdefault/default")
     bfsloc = bucketfs_location(params)
     print(f'{params}')
 
@@ -37,14 +44,37 @@ def upload_to_bucketfs(file: str):
     for f in bfsloc.iterdir():
         print(f'{f}')
 
+def list_bfs():
+    from mlflow.entities import FileInfo
+    bfsloc = bfs_location("exa+bfs://localhost:2580/bfsdefault/default")
+    path = None
+    result = []
+
+    def info(root: bfs.path.PathLike, name: str, is_dir: bool):
+        full_path = (path / root if path else root) / name
+        print(f'listing {full_path}')
+        return FileInfo(path=full_path, is_dir=is_dir, file_size=None)
+
+    def dir_info(root: bfs.path.PathLike, name: str):
+        return info(root, name, is_dir=True)
+
+    def file_info(root: bfs.path.PathLike, name: str):
+        return info(root, name, is_dir=False)
+
+    for root, dirs, files in bfsloc.walk():
+        result += [dir_info(root, x) for x in dirs]
+        result += [file_info(root, x) for x in files]
+
+    return result
+
 
 def main():
     store_model()
     return
-    import sys
-    file = sys.argv[1]
-    print(f'{file}')
-    upload_to_bucketfs(file)
+    result = list_bfs()
+    fi = FileInfo("abc", is_dir=False, file_size=None)
+    print(f'{fi.path}')
+    print(f'{fi.to_proto()}')
 
 
 if __name__ == "__main__":
