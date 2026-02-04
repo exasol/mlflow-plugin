0499d3f281b437efe8e54a3af4c1e0e219dec012
ckunki <christoph.kuhnke@exasol.com>
2026-02-04 17:36:58 +0100

    Formatting

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,4 +1,6 @@
 import subprocess
 
+
 def test_poetry_version() -> None:
     subprocess.run(["poetry", "--version"])
+    subprocess.run(["poetry", "export", "--without-hashes", "--without-urls"])
