1fdf6689dc7183bdbda13ce30b0f60ffb7991a08
ckunki <christoph.kuhnke@exasol.com>
2026-02-05 13:45:10 +0100

    Removed unused context manager export_slc

--- exasol/mlflow_plugin/slc.py
+++ exasol/mlflow_plugin/slc.py
@@ -14,13 +14,3 @@ def slc_build_context():
         project_directory = find_path_backwards("pyproject.toml", __file__).parent
         builder.prepare_flavor(project_directory)
         yield builder
-
-
-# @contextmanager
-# def slc_export_context(export_dir: str | None = None):
-#     if export_dir and (not os.path.isdir(export_dir)):
-#         os.makedirs(export_dir)
-#     with slc_build_context() as builder:
-#         result = builder.export(export_dir)
-#         info = result.export_infos[str(builder.flavor_path)]["release"]
-#         yield info.cache_file
