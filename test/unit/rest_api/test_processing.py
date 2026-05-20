e203f45b0d5db62aa8590923f8046092731cf891
ckunki <christoph.kuhnke@exasol.com>
2026-05-20 18:26:21 +0200

    Added unit tests for PostProcessor

---
+++ test/unit/rest_api/test_processing.py
@@ -0,0 +1,36 @@
+import pytest
+
+from exasol.mlflow_plugin.rest_api.column import Column
+from exasol.mlflow_plugin.rest_api.processing import PostProcessor
+
+
+def test_no_expansion() -> None:
+    testee = PostProcessor(
+        has_tags=False,
+        columns=[
+            Column("c1", 10),
+            Column("c2", 10),
+        ],
+    )
+    input = [
+        {"c1": 1, "c2": 2},
+        {"c1": 3, "c2": 4},
+    ]
+    assert list(testee.process(input)) == [[1, 2], [3, 4]]
+
+
+def test_expansion() -> None:
+    testee = PostProcessor(has_tags=True, columns=[Column("c1", 10)])
+    tags = [
+        {"key": "K1", "value": "V1"},
+        {"key": "K2", "value": "V2"},
+    ]
+    input = [
+        {"c1": 1, "tags": tags},
+        {"c1": 2}
+    ]
+    assert list(testee.process(input)) == [
+        [1, "K1", "V1"],
+        [1, "K2", "V2"],
+        [2, None, None],
+    ]
