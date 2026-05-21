4863ea1a3c7b84c667ccbd07bd7579a6a4eafea8
ckunki <christoph.kuhnke@exasol.com>
2026-05-21 17:23:05 +0200

    Fixed attribute

--- exasol/mlflow_plugin/rest_api/processing.py
+++ exasol/mlflow_plugin/rest_api/processing.py
@@ -24,7 +24,7 @@ class PostProcessor:
 
     def __init__(self, columns: list[Column], expanders: list[Expander] | None = None):
         self.expanders = expanders or []
-        self.columns = columns + [c for e in self.expanders for c in e.columns]
+        self.columns = columns + [c for e in self.expanders for c in e.output_columns]
 
     def process(self, data: Iterable[JsonObject]) -> Iterable[list[Any]]:
         for expander in self.expanders:
