0e1b469fa33c0ca2683c0b078eee9ee1ee9bf55d
ckunki <christoph.kuhnke@exasol.com>
2026-06-01 08:29:34 +0200

    Removed obsolete nox session

--- noxfile.py
+++ noxfile.py
@@ -19,26 +19,3 @@ def slc_export(session: nox.Session):
     export_path = PROJECT_CONFIG.root_path / ".slc"
     with slc_build_context() as builder:
         builder.export(export_path)
-
-
-@nox.session(name="slc:activation", python=False)
-def slc_activation(session: nox.Session):
-    """
-    Display the activation command for the SLC.
-    """
-    export_path = PROJECT_CONFIG.root_path / ".slc"
-    suffix = ".tar.gz"
-    stem = next(export_path.glob(f"*{suffix}")).name.removesuffix(suffix)
-    bfspath = "bfsdefault/default"
-    prefix = "PYTHON3=builtin_python3 R=builtin_r JAVA=builtin_java"
-    udf_client_binary = "udfclient"
-    sql = (
-        "ALTER SYSTEM SET SCRIPT_LANGUAGES"
-        f"='{prefix} MLFLOW=localzmq+protobuf:///{bfspath}/{stem}?"
-        f"lang=python#buckets/{bfspath}/{stem}/exaudf/{udf_client_binary}';"
-    )
-    print(
-        "Use the following SQL command",
-        "to active the Script language alias MLFLOW:\n\n",
-        sql
-    )
