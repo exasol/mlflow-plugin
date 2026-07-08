2c459fcae44f85d8c07b05c2fcbb86945db31eda
ckunki <christoph.kuhnke@exasol.com>
2026-07-08 09:27:25 +0200

    #84: Render `Column` as json as required for Virtual Schema API

--- exasol/mlflow_plugin/rest_api/data/column.py
+++ exasol/mlflow_plugin/rest_api/data/column.py
@@ -5,6 +5,8 @@ from datetime import (
 )
 from typing import Any
 
+JsonObject = dict[str, Any]
+
 
 def timestamp_to_datetime(seconds_since_epoc: int) -> datetime:
     """
@@ -50,15 +52,43 @@ class Column:
         self.comma_sep = comma_sep
 
     @property
+    def _sql_data_type(self) -> str:
+        return SQL_TYPE.get(self.data_type, "VARCHAR")
+
+    @property
+    def _json_data_type(self) -> JsonObject:
+        def data_type():
+            yield "type", self._sql_data_type
+            if self.data_type == int:
+                yield "precision", self.size
+                yield "scale", 0
+            if self.data_type == str:
+                yield "size", self.size
+
+        return dict(data_type())
+
+    @property
+    def json(self) -> JsonObject:
+        """
+        Returns a JsonObject describing the column as required for Virtual
+        Schema API.
+        """
+        return {"name": self.sql_name, "dataType": self._json_data_type}
+
+    @property
     def sql_type(self) -> str:
-        prefix = SQL_TYPE.get(self.data_type, "VARCHAR")
-        if self.data_type in [str, datetime]:
-            suffix = f"({self.size})"
-        elif self.data_type == int:
-            suffix = f"({self.size},0)"
-        else:
-            suffix = ""
-        return f"{prefix}{suffix}"
+        def suffix() -> str:
+            jdt = self._json_data_type
+            if size := jdt.get("size"):
+                return f"({size})"
+            if self.data_type == datetime:
+                return "(3)"
+            if precision := jdt.get("precision"):
+                scale = jdt.get("scale", 0)
+                return f"({precision},{scale})"
+            return ""
+
+        return f"{self._sql_data_type}{suffix()}"
 
     @property
     def sql(self) -> str:
