2b53280e372319bc1cc3c218211c28a8d417b3b5
ckunki <christoph.kuhnke@exasol.com>
2026-07-14 13:45:59 +0200

    Fixed review findings
    used fixtures in integration test

--- test/integration/virtual_schema/test_virtual_schema.py
+++ test/integration/virtual_schema/test_virtual_schema.py
@@ -1,6 +1,7 @@
 import importlib.resources
 
 import pyexasol
+import pytest
 from exasol.pytest_slc import udf_debug
 
 from exasol.mlflow_plugin.rest_api.virtual_schema import (
@@ -8,45 +9,48 @@ from exasol.mlflow_plugin.rest_api.virtual_schema import (
     VirtualSchema,
 )
 
-RESOURCES = importlib.resources.files("test.integration.virtual_schema")
+
+@pytest.fixture
+def vs_adapter(db_schema_name, pyexasol_connection):
+    adapter_impl = (
+        importlib.resources.files("test.integration.virtual_schema.resources")
+        / "adapter_impl.py"
+    ).read_text()
+    return Adapter(
+        db_schema_name, "VS_ADAPTER", adapter_impl, language_alias="PYTHON3"
+    )
 
 
-def drop_virtual_schema(vs: VirtualSchema, con: pyexasol.ExaConnection):
-    """
-    The adapter <A> is created in DB schema db_schema_name <S>.
+@pytest.fixture
+def virtual_schema(vs_adapter, pyexasol_connection):
+    properties = {"CONNECTION_NAME": "MLFLOW", "MAX_RESULTS": "100"}
+    virtual_schema = VirtualSchema("MLFLOW_VS", vs_adapter, properties)
+    pyexasol_connection.execute("ALTER SESSION SET SCRIPT_OUTPUT_ADDRESS=''")
+    try:
+        yield virtual_schema
+    finally:
+        # The adapter <A> is created in DB schema db_schema_name <S>.
+        #
+        # The VS schema needs to be dropped before fixture pyexasol_connection
+        # from pytest-extension drops the <S> when the test session has
+        # finished to avoid the following error
+        #
+        # The schema <S>_name contains the Adapter Script <A> for which
+        # at least one Virtual Schema exists (MLFLOW_VS).  Please drop all
+        # Virtual Schemas of this Adapter first.
+        virtual_schema.drop(pyexasol_connection)
 
-    The VS schema needs to be dropped before fixture pyexasol_connection
-    from pytest-extension drops the <S> when the test session has
-    finished to avoid the following error
 
-    The schema <S>_name contains the Adapter Script <A> for which
-    at least one Virtual Schema exists (MLFLOW_VS).  Please drop all
-    Virtual Schemas of this Adapter first.
-    """
-
-    vs.drop(con)
-
-
-def test_adapter(db_schema_name, pyexasol_connection) -> None:
+def test_adapter(db_schema_name, pyexasol_connection, virtual_schema) -> None:
     def query_func(sql: str) -> udf_debug.QueryResult:
         stmt = pyexasol_connection.execute(sql)
         return [] if stmt.rowcount() == 0 else stmt.fetchall()
 
-    con = pyexasol_connection
-    adapter_impl = (RESOURCES / "adapter_impl.py").read_text()
-    adapter = Adapter(
-        db_schema_name, "VS_ADAPTER", adapter_impl, language_alias="PYTHON3"
-    )
-    properties = {"CONNECTION_NAME": "MLFLOW", "MAX_RESULTS": "100"}
-    vs = VirtualSchema("MLFLOW_VS", adapter, properties)
-    vs.drop(con)
     pipe = udf_debug.LogPipe()
     expected = [
         "Adapter call: createVirtualSchema",
-        f"properties: {properties}",
-        "Adapter call: dropVirtualSchema",
+        f"properties: {virtual_schema.properties}",
     ]
     with udf_debug.UdfOutputLogger(query=query_func, print_func=pipe.input):
-        vs.create(con)
-        drop_virtual_schema(vs, con)
+        virtual_schema.create(pyexasol_connection, replace=True)
         udf_debug.wait_for_messages(pipe.output, *expected)
